a0ded624d550a525d556267ab46dcfcd8a6aeb55
Roby Hartono <robyhartonodev312@gmail.com>
2023-02-05 11:46:26 +0100

    Updated added initial sql script setp for tables, h2 database

---
+++ src/main/resources/db/migration/V1__Initialization.sql
@@ -0,0 +1,21 @@
+create table "customers"
+(
+    "id"    bigserial primary key not null,
+    "name"  varchar(255)          not null,
+    "email" varchar(255)          not null
+);
+
+create table "orders"
+(
+    "id"              bigserial primary key not null,
+    "subject"         varchar(255)          not null,
+    "images"          json null,
+    "order_date"      date                  not null,
+    "order_date_time" timestamp(0) without time zone not null,
+    "customer_id"     bigint                not null,
+    "created_at"      timestamp(0) without time zone null,
+    "updated_at"      timestamp(0) without time zone null
+);
+
+alter table "orders"
+    add constraint "orders_customer_id_foreign" foreign key ("customer_id") references "orders" ("id") on delete cascade on update cascade;
