ecbef21223dd7a4e342ee40ee550b13a7387de5f
Roby Hartono <robyhartonodev312@gmail.com>
2023-02-06 13:06:42 +0100

    Optimize route handling for SPA

--- src/main/java/com/empulse/assignment/controller/ApplicationController.java
+++ src/main/java/com/empulse/assignment/controller/ApplicationController.java
@@ -1,27 +1,13 @@
 package com.empulse.assignment.controller;
 
-import com.empulse.assignment.model.Customer;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@Slf4j
 public class ApplicationController {
 
-    @RequestMapping(value="{_:^(?!index\\.html|api).*$}")
-    public String get(){
-        return "forward:/";
-    }
-
-    @GetMapping("/lmao")
-    public void test() {
-        Customer customer = new Customer();
-        customer.setName("roby");
-        customer.setEmail("roby@gmx.de");
-
-        log.info("test123");
-        log.info(customer.getName());
+    @RequestMapping(value = {"/{path:^(?!api|public)[^\\.]*}", "/**/{path:^(?!api|public).*}/{path:[^\\.]*}"})
+    public String spa() {
+        return "forward:/index.html";
     }
 }
